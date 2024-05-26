7a387eec38c8dc194a85788e3c8c46f65ea6b8bf
Maki0806 <masaki.onuki.0806@gmail.com>
2024-05-27 02:16:54 +0900

    Add feature making gsp filter design and saving filter response

--- GSP_TRAFFIC_Python/src/util.py
+++ GSP_TRAFFIC_Python/src/util.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.sparse import find
 from typing import List
 from pathlib import Path
+from pygsp import filters
 
 
 def top_dir() -> Path:
@@ -15,13 +16,16 @@ def draw_graph(
     pos: np.ndarray,
     data: np.ndarray = None,
     image: str = None,
+    fig_title: str = None,
     use_node_values: np.ndarray = None,
     node_value_disabled: bool = False,
 ):
     position_dict = {
         node_num: (pos_x, pos_y) for node_num, (pos_x, pos_y) in enumerate(pos)
     }
-    node_value_disabled = _check_node_value_disabled(data, use_node_values)
+    node_value_disabled = _check_node_value_disabled(
+        data, use_node_values, node_value_disabled
+    )
     if not node_value_disabled and use_node_values is None:
         use_node_values = data
     node_sizes = _make_node_size(data, node_value_disabled)
@@ -33,11 +37,14 @@ def draw_graph(
         node_sizes=node_sizes,
         position_dict=position_dict,
         save_image_name=image,
+        fig_title=fig_title,
         node_value_disabled=node_value_disabled,
     )
 
 
-def _check_node_value_disabled(data, use_node_values) -> bool:
+def _check_node_value_disabled(data, use_node_values, node_value_disabled) -> bool:
+    if node_value_disabled:
+        return node_value_disabled
     node_value_disabled = data is None and use_node_values is None
     if data is None and use_node_values is not None:
         raise Exception("Please input `data` into draw_graph to show the node values.")
@@ -75,6 +82,7 @@ def _save_graph(
     node_sizes: List[float],
     position_dict: dict,
     save_image_name: str,
+    fig_title: str,
     node_value_disabled: bool,
 ) -> None:
     fig = plt.figure(figsize=(10, 10))
@@ -96,6 +104,8 @@ def _save_graph(
         sm.set_array([0, max(use_node_values)])
         fig.colorbar(sm, ax=ax)
     nx.draw_networkx_edges(G=graphD, pos=position_dict, width=0.2)
+    if fig_title:
+        ax.set_title(fig_title)
     fig.tight_layout()
     if isinstance(save_image_name, str):
         save_path = top_dir() / save_image_name
@@ -109,11 +119,16 @@ def apply_gft_to_signal(G, graph_signal: np.ndarray):
     return gft_signal
 
 
-def save_gs_spectrum(gft_signal: np.ndarray, save_image_name: str):
+def save_gs_spectrum(
+    gft_signal: np.ndarray, save_image_name: str, fig_title: str = None
+):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(1, 1, 1)
     signal_index = [i for i in range(len(gft_signal))]
     ax.stem(signal_index, np.abs(gft_signal))
+    if fig_title:
+        ax.set_title(fig_title)
+    fig.tight_layout()
     if isinstance(save_image_name, str):
         save_path = top_dir() / save_image_name
         fig.savefig(save_path, dpi=500)
@@ -124,3 +139,24 @@ def normalize_graph_signal(graph_signal: np.ndarray, axis: int = 0) -> np.ndarra
     mean_gs = np.mean(graph_signal, axis=axis, keepdims=True)
     std_gs = np.std(graph_signal, axis=axis, keepdims=True)
     return (graph_signal - mean_gs) / std_gs
+
+
+def gsp_design_smooth_indicator(G, a1, a2):
+    G.compute_fourier_basis()
+    lmax = G.lmax
+    fx = lambda x, a: np.exp(-a / x) * (x >= 0)
+    gx = lambda x, a: fx(x, a) / (fx(x, a) + fx(1 - x, a))
+    ffin = lambda x, a1, a2: gx(1 - (x - a1) / (a2 - a1), 1) * (x >= a1)
+    g = lambda x: ffin(x / lmax, a1, a2)
+    return filters.Filter(G, g)
+
+
+def save_filter(g, filter_name: str, fig_title: str = None):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(1, 1, 1)
+    g.plot(plot_eigenvalues=True, ax=ax)
+    if fig_title:
+        ax.set_title(fig_title)
+    save_path = top_dir() / filter_name
+    fig.tight_layout()
+    plt.savefig(save_path)
